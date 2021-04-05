0399ae6c093f2ad7f095d5e8c29c4ed9c2dfb31d
Raymart Jay Canoy <31064209+rjcanoy03@users.noreply.github.com>
2021-04-06 02:01:33 +0900

    Add files via upload

--- prob4a.m
+++ prob4a.m
@@ -10,7 +10,7 @@ x1 = exp(-t1).*sin(20*pi*t1) + exp(-t1/2).*sin(19*pi*t1);
 
 figure;
 subplot(1, 2, 1)
-p = plot(t, x1, 'k');
+p = plot(t1, x1, 'k');
 set(p, 'LineWidth', 1)
 xlabel('$t$', 'Interpreter', 'latex');
 ylabel('$x_{1}(t)$', 'Interpreter', 'latex');
@@ -20,7 +20,7 @@ title('x_1(t) = exp(-t)sin(20\pit) + exp(-t/2)sin(19\pit)', 'FontName', 'Times',
 %% Part 2: x_{2}(t) = rect(t)*cos(20*pi*t)
 fs = 500;
 t2 = [-2: 1/fs : 6];
-x2 = prob4a_rect(t2).*cos(20*pi*t);
+x2 = prob4a_rect(t2).*cos(20*pi*t2);
 
 subplot(1, 2, 2)
 p = plot(t2, x2, 'k');
